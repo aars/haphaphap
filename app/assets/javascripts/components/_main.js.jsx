c10d87f9df2926fe0170da4e3aa7c158127e34d4
Aaron <aaronheesakkers@gmail.com>
2019-04-06 20:50:01 +0200

    initial commit

---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,16 @@
+const styles = {
+    margin: "1em",
+    padding: "1em",
+    border: "1px solid #ddd"
+  };
+  
+  const Main = props => {
+    return (
+      <div style={styles}>
+        <center>
+          <h1>Hap Hap Hap!</h1>
+        </center>
+        <p> Mac 'n Cheese </p>
+      </div>
+    );
+  };
